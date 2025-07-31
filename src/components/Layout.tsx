3a59d60f6832b1b75f0ea0dda116471b72a7904e
Riya-dev185 <t8183636@gmail.com>
2025-07-31 23:29:03 +0530

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -51,8 +51,10 @@ const Layout: React.FC<LayoutProps> = ({
       <main className="max-w-7xl mx-auto px-4 py-6">
         {children}
       </main>
+    }
     </div>
   );
+}
 };
 
 export default Layout;
